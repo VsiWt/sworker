84e3e4ef752af88d250a6d62e27e2822107f97f8
gyzhang <guiyong.zhang@verisilicon.com>
2022-07-22 18:09:23 +0800

    use mirror instead of github

--- sworker.sh
+++ sworker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 root_dir=`pwd`
-github_user=GYZHANG2019
 gerrit_user=cn1208
 
+set -o pipefail
 function create_folder(){
     folder=fork_`date "+%m%d%H%M"`
     mkdir $folder &&
@@ -14,19 +14,20 @@ function create_folder(){
 
 function clone_amd_gits(){
     cd $root_dir;
+    amd_branch=prototype_production
 
     rm ma35_vsi_libs ma35_ffmpeg ma35_linux_kernel ma35 -rf
-    echo "clone ma35_vsi_libs.git from github..." &&
-    git clone git@github.com:$github_user/ma35_vsi_libs.git -b prototype_production &&
+    echo "clone ma35_vsi_libs.git from mirror github..."
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/github/Xilinx-Projects/ma35_vsi_libs" -b $amd_branch
 
-    echo "clone ma35_ffmpeg.git from github..." &&
-    git clone git@github.com:$github_user/ma35_ffmpeg.git -b prototype_production &&
+    echo "clone ma35_ffmpeg.git from github..."
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/github/Xilinx-Projects/ma35_ffmpeg" -b $amd_branch
 
-    echo "clone ma35_linux_kernel.git from github..." &&
-    git clone git@github.com:$github_user/ma35_linux_kernel.git -b prototype_verification &&
+    echo "clone ma35_linux_kernel.git from github..."
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/github/Xilinx-Projects/ma35_linux_kernel" -b $amd_branch
 
-    echo "clone ma35.git from github..." &&
-    git clone git@github.com:$github_user/ma35.git &&
+    echo "clone ma35.git from github..."
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/github/Xilinx-Projects/ma35" -b develop
 
     echo -e "done"
 }
@@ -35,20 +36,20 @@ function clone_vsi_gits(){
 
     echo "clone ffmpeg from VSI gerrit..."
     cd $root_dir;
-    cd ma35_ffmpeg/ && rm src -rf &&
-    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/ffmpeg/ffmpeg" src -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "src/.git/hooks/" &&
-    echo -e "done" &&
+    cd ma35_ffmpeg/ && rm src -rf
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/ffmpeg/ffmpeg" src -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "src/.git/hooks/"
+    echo -e "done"
 
-    echo "clone drivers from VSI gerrit..." &&
-    cd $root_dir &&
-    cd ma35_linux_kernel/ && rm src -rf &&
-    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/gitlab/Transcoder/drivers" src -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "src/.git/hooks/" &&
-    echo -e "done" &&
+    echo "clone drivers from VSI gerrit..."
+    cd $root_dir
+    cd ma35_linux_kernel/ && rm src -rf
+    git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/gitlab/Transcoder/drivers" src -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "src/.git/hooks/"
+    echo -e "done"
 
-    cd $root_dir &&
-    cd ma35_vsi_libs/src &&
-    rm vpe common VC8000D VC8000E build VIP2D drivers -rf &&
-    echo "clone vsi libs from VSI gerrit..." &&
+    cd $root_dir
+    cd ma35_vsi_libs/src
+    rm vpe common VC8000D VC8000E build VIP2D drivers -rf
+    echo "clone vsi libs from VSI gerrit..."
     git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/gitlab/Transcoder/common" -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "common/.git/hooks/"
     git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/gitlab/Transcoder/VC8000D" -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "VC8000D/.git/hooks/"
     git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/gitlab/Transcoder/VC8000E" -b spsd/master && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "VC8000E/.git/hooks/"
@@ -56,21 +57,21 @@ function clone_vsi_gits(){
     git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/VSI/SDK/transcoding" build -b master && scp -p -P $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "build/.git/hooks/"
     git clone "ssh://$gerrit_user@gerrit-spsd.verisilicon.com:29418/VSI/GAL/driver" VIP2D -b spsd/SuperNova && scp -p -P 29418 $gerrit_user@gerrit-spsd.verisilicon.com:hooks/commit-msg "VIP2D/.git/hooks/"
 
-    echo "Link ffmpeg from ma35_ffmpeg to ma35_vsi_libs/src/ffmpeg" &&
-    cd $root_dir &&
-    cd ma35_vsi_libs/src/ &&
-    ln -s ../../ma35_ffmpeg/src/ ffmpeg &&
+    echo "Link ffmpeg from ma35_ffmpeg to ma35_vsi_libs/src/ffmpeg"
+    cd $root_dir
+    cd ma35_vsi_libs/src/
+    ln -s ../../ma35_ffmpeg/src/ ffmpeg
 
-    echo "Link driver from ma35_linux_kernel to ma35_vsi_libs/src/drivers" &&
+    echo "Link driver from ma35_linux_kernel to ma35_vsi_libs/src/drivers"
     ln -s ../../ma35_linux_kernel/src/ drivers
 }
 
 function build(){
     echo "Building full project with CMake..."
-    cd $root_dir  &&
-    rm build -rf && mkdir build && cd build &&
-    cmake ../ma35 -G Ninja -DCMAKE_BUILD_TYPE=Debug -DMA35_FORCE_NO_PRIVATE_REPOS=true -DREPO_USE_LOCAL_vsi_libs=true -DREPO_USE_LOCAL_linux_kernel=true -DREPO_USE_LOCAL_ffmpeg=true &&
-    ninja ffmpeg_vsi sn_int &&
+    cd $root_dir
+    rm build -rf && mkdir build && cd build
+    cmake ../ma35 -G Ninja -DCMAKE_BUILD_TYPE=Debug -DMA35_FORCE_NO_PRIVATE_REPOS=true -DREPO_USE_LOCAL_vsi_libs=true -DREPO_USE_LOCAL_linux_kernel=true -DREPO_USE_LOCAL_ffmpeg=true
+    ninja ffmpeg_vsi sn_int
     echo -e "done"
 }
 
@@ -114,7 +115,6 @@ function package(){
 
 function help(){
     echo "this script will pull both AMD gits and/or VSI gits, and do compiling, finally generate test package"
-    echo "$0 --github_user=:  set the github account wich contains AMD gits"
     echo "$0 --gerrit_user=:  set the gerrit account wich contains VSI gits"
     echo "$0 new_project:     create one new rmpty project."
     echo "$0 clone_amd_gits:  clone AMD gits only."
@@ -129,9 +129,6 @@ for (( i=1; i <=$#; i++ )); do
     next_opt=$((i+1))
     next_value=${!next_opt}
     case "$opt" in
-    --github_user=*)
-        echo "github_user=$optarg"
-        github_user=$optarg;;
     --gerrit_user=*)
         echo "gerrit_user=$optarg"
         gerrit_user=$optarg;;
